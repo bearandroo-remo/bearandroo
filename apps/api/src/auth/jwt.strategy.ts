6e845204e15058e1b8ac93214ae39b86d07d66f9
yunusoncel <yunusoncel@gmail.com>
2026-06-06 15:44:30 +0300

    update validate

--- apps/api/src/auth/jwt.strategy.ts
+++ apps/api/src/auth/jwt.strategy.ts
@@ -12,7 +12,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: { sub: string; email: string; role: string }) {
+  validate2(payload: { sub: string; email: string; role: string }) {
     return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
+
+  validate(payload: { sub: string; email: string; role: string }) {
+    return { sub: payload.sub, email: payload.email, role: payload.role };
   }
 }
